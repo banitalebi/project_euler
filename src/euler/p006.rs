c120e521a6b27960ee034a42d759b2f960308bd9
banitalebi <banitalebi.akbar@gmail.com>
2023-12-09 23:46:21 +0330

    Problem 6: sum square difference

---
+++ src/euler/p006.rs
@@ -0,0 +1,8 @@
+#[allow(dead_code)]
+pub fn problem_6() {
+    // Problem 6: sum square difference
+    // https://projecteuler.net/problem=6
+    let num = 100;
+    let s = (num -1) * num * (num +1) * (3*num + 2) / 12;    
+    println!("Problem 6: sum square difference, {}", s);
+}
--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,6 @@ fn main() {
     // euler::p002::problem_2();
     // euler::p003::problem_3();
     // euler::p004::problem_4();
-    euler::p005::problem_5();
+    // euler::p005::problem_5();
+    euler::p006::problem_6();
 }
