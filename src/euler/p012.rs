bd17ba57dda809cfc1f89844aaa9244d7103c5eb
banitalebi <banitalebi.akbar@gmail.com>
2023-12-15 14:15:27 +0330

    Problem 12: highly divisible triangular number

--- src/euler/p012.rs
+++ src/euler/p012.rs
@@ -2,36 +2,43 @@
 pub fn problem_12() {
     // Problem 12: highly divisible triangular number
     // https://projecteuler.net/problem=12
-    let mut triangular:u32 = 0;    
-    let mut natural:u32 = 1;    
+    let mut triangular:u64 = 0;
+    let mut i:u64 = 1;
+    let mut count:u32 = 0;
     loop{
-        triangular+=natural;
-        if count_factors(triangular)>=500{
-            break;        
+        triangular+=i;
+        count = count_factors(triangular);
+        if count>500{
+            break;
         }
-        natural+=1;
-    }    
+        i+=1;
+    }
     println!("Problem 12: highly divisible triangular number, {}", triangular);
 }
 
-fn count_factors(number: u32)->u32{
+fn count_factors(number: u64)->u32{
     if number==1{
         return 1;
     }
-    let mut _tau:u32=1;
-    let mut _n:u32=number;
-    let mut _i:u32=2;
-    while _i*_i<=_n {        
-        let mut count:u32=1;
-        while _n%_i==0{
-            _n/=_i;
-            count+=1;
+    let mut count: u32 = 0;
+    for i in 1..number{
+        if i*i>number{
+            break;
         }
-        _tau*=count;                
-        _i+=1;
+        if number%i==0{
+            count +=2;
+        }
     }
-    if (_n==number)||(_n>1){
-        _tau+=1;
-    }
-    return _tau;
+    return count;
+}
+
+#[test]
+fn test_count_factors(){
+    assert_eq!(count_factors(1), 1);
+    assert_eq!(count_factors(3), 2);
+    assert_eq!(count_factors(6), 4);
+    assert_eq!(count_factors(10), 4);
+    assert_eq!(count_factors(15), 4);
+    assert_eq!(count_factors(21), 4);
+    assert_eq!(count_factors(28), 6);
 }
