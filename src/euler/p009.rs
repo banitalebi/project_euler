fc47884e169e517bd79097afca7a10f9a6cda72c
banitalebi <banitalebi.akbar@gmail.com>
2023-12-16 01:13:47 +0330

    Problem 9: special pythagorean triplet

--- src/euler/p009.rs
+++ src/euler/p009.rs
@@ -3,15 +3,15 @@ pub fn problem_9() {
     // Problem 9: Special Pythagorean Triplet
     // https://projecteuler.net/problem=9
     let mut p =0;
-    for a in 1..498{
-        for b in a..499{
-            for c in b..500{
-                if a+b+c==1000{
-                    if a*a+b*b==c*c{
-                        p=a*b*c;
-                    }
-                }
+    for a in 1..333{
+        for b in a+1..500{
+            let c = 1000 - ( a + b );
+            if c<=b{
+                continue;
             }
+            if a*a+b*b==c*c{
+                p=a*b*c;
+            }         
         }
     }      
     println!("Problem 9: special pythagorean triplet, {}", p);
