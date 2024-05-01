af1d09897bf7f4d1e0288501d13850cca69f090f
Akbar Banitalebi <banitalebi.akbar@gmail.com>
2024-05-01 03:59:00 +0330

    improving problem001

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,11 @@ pub mod problem001 {
     pub fn run() -> u32 {
         // Problem 1: Multiples of 3 or 5
         // https://projecteuler.net/problem=1
-        let sum:u32 = 3 * arithmetic_progression(333) + 
-                      5 * arithmetic_progression(199) - 
-                      15 * arithmetic_progression(66);
-        sum
-    }
-    
-    fn arithmetic_progression(n: u32)->u32{
-        return n * (n + 1) /2;
+        let arithmetic_progression = |n:u32| n * (n + 1)/2;
+        3 * arithmetic_progression(333) + 
+        5 * arithmetic_progression(199) - 
+        15 * arithmetic_progression(66)
+        
     }
 }
 
